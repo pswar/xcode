4414c10c38b083209283231e6555a38b33083b41
pswar <sathish@pottavathini.com>
2015-09-30 12:01:52 -0700

    More updates

--- Housie Board/Housie Board/Housie Board/SecondViewController.swift
+++ Housie Board/Housie Board/Housie Board/SecondViewController.swift
@@ -21,16 +21,20 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //createButtons()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6;
+        return 5;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "My test cell");
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell_\(indexPath.row)");
         let tot = getDouble(textAmount.text!);
         
         var perc:Double;
@@ -65,21 +69,18 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         }
         
         cell.textLabel?.text = txt;
+        cell.backgroundColor = (indexPath.row%2==0) ? UIColorFromRGB("FACC2E"): UIColorFromRGB("D8D8D8");
         return cell;
     }
     
-    func toMoney(val:Double) -> String {
-        return String(format: "$ %.2f", val)
+    @IBAction func calcAmount(sender: AnyObject) {
+        tableView.reloadData();
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func calcAmount(sender: AnyObject) {
-        
-        tableView.reloadData();
+    //====Utilities====
+    
+    func toMoney(val:Double) -> String {
+        return String(format: "$ %.2f", val)
     }
     
     func getDouble(text:String!) -> Double {
